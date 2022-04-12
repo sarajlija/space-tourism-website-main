6c89d818351c4719938a32e445e64c59b952986f
Salko <64722632+sarajlija@users.noreply.github.com>
2022-04-12 13:17:37 +0200

    update destination
    animation ifinity, add active css

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
-fetch("./data.json")
-.then(function(resp){
-    return resp.json
-})
-.then(function(data){
-console.log(data)
-}) 
+/*
+// Get the container element
+var ulContainer = document.getElementById("ul-id");
 
+// Get all buttons with class="btn" inside the container
+var links = ulContainer.getElementsByClassName("nav-li");
 
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function (e) {
+       // e.preventDefault()
+        var current = document.getElementsByClassName("active");
 
+        // If there's no active class
+        if (current.length > 0) {
+            current[0].className = current[0].className.replace(" active", "");
+        }
+
+        // Add the active class to the current/clicked button
+        this.className += " active";
+    });
+}*/
